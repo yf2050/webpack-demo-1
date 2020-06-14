5ec210554b40ab368296d86c5e327165da27e03d
YuFeng <1429483153@qq.com>
2020-06-14 18:53:28 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import x from "./x.js";
+import png from "./assets/1.jpg";
+const div = document.getElementById("app");
+div.innerHTML = `<img src='${png}'>`;
+console.log(div.innerHTML);
 console.log("hi");
 console.log("hi2");
 console.log(x);
+
+//懒加载
+
+const button = document.createElement("button")
+button.innerText = "懒加载"
+button.onclick = () => {
+    const promise = import('./lazy.js')
+    console.log(promise.then((fn) => {
+        console.log(fn)
+    }))
+}
+document.body.appendChild(button)
